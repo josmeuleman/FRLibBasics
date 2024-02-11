168cfce10fcf8b0f52c8d573992b42984cafa9b7
josmeuleman <josmeuleman@gmail.com>
2024-02-11 22:06:57 +0100

    FRPPMReceiverSensor added

---
+++ src/FRPPMReceiverSensor.cpp
@@ -0,0 +1,96 @@
+// Class for receiving PPM signals, and add it to logger
+// 
+// 2023-04-16, Jos Meuleman, Inholland Aeronautical & Precision Engineering, The Netherlands
+
+#include "Arduino.h"
+#include "FRPPMReceiverSensor.h"
+#include "FRGeneric.h"
+
+PPMReceiverSensor* PPMReceiverSensor::_instance = nullptr;
+
+volatile unsigned long PPMReceiverSensor::_lastPulseUS;
+volatile uint16_t PPMReceiverSensor::_channel;
+volatile int* PPMReceiverSensor::_channelValues;
+
+PPMReceiverSensor::PPMReceiverSensor(int pinNumber, int numberOfChannels) {
+  _instance = this; // Store a pointer to this instance
+  _numberOfChannels = numberOfChannels;
+  _pinNumber = pinNumber;
+ 
+  if (_channelValues != 0) {
+    delete[] _channelValues;
+  }
+  _channelValues = new int[_numberOfChannels];
+  
+  for (int i = 0; i < _numberOfChannels; i++) {
+	_channelValues[i] = 0;
+  }
+  _channel = 0;
+}
+
+PPMReceiverSensor::~PPMReceiverSensor() {
+  detachInterrupt(_pinNumber);
+  delete[] _channelValues;
+}
+
+void PPMReceiverSensor::Init(){
+  pinMode(_pinNumber, INPUT);
+  attachInterruptArg(digitalPinToInterrupt(_pinNumber), ppm_isr, this, CHANGE);
+  _lastPulseUS = micros();
+}
+
+
+void PPMReceiverSensor::SetPrefix(String prefix) {
+  _prefix = prefix; 
+}
+
+int PPMReceiverSensor::ReadChannel(int ChannelNumber) {
+  if ((ChannelNumber >= 0) && (ChannelNumber < _numberOfChannels)) {
+    return (_channelValues[ChannelNumber]);
+  } 
+  else {
+    return (0);
+  }
+}
+
+String PPMReceiverSensor::HeaderString(){
+  String tempString;
+  for (int i = 0; i < _numberOfChannels; i++) {
+	tempString.concat(_prefix);
+	tempString.concat(i);
+	tempString.concat("; ");
+  }
+  return tempString;
+}
+
+String PPMReceiverSensor::SensorString(){
+  String tempString;
+  for (int i = 0; i < _numberOfChannels; i++) {
+	tempString.concat(createFloatString(_channelValues[i], 2));  
+  }
+  return tempString;
+}
+
+void PPMReceiverSensor::CountPulse() {
+  unsigned long tNow = micros();
+  if (digitalRead(_pinNumber)) {
+    _lastPulseUS = micros();
+  } else {
+    long x = tNow - _lastPulseUS;
+    _lastPulseUS = tNow;
+    if (x > _PULSETHRESHUS) {
+      _channel = 0;
+    }
+    else {
+	  if (_channel < _numberOfChannels) {
+	    _channelValues[_channel] = x;
+        _channel++;
+	  }
+	}
+  }  
+}
+
+void IRAM_ATTR PPMReceiverSensor::ppm_isr(void* arg) {
+  PPMReceiverSensor* receiver = reinterpret_cast<PPMReceiverSensor*>(arg);
+  receiver->CountPulse();
+}
---
+++ src/FRPPMReceiverSensor.h
@@ -0,0 +1,44 @@
+
+// Class for receiving PPM signals
+// 
+// 2023-03-19, Jos Meuleman, Inholland Aeronautical & Precision Engineering, The Netherlands
+
+#ifndef FRPPMReceiverSensor_h
+#define FRPPMReceiverSensor_h
+
+#include <FRSensorManager.h>
+
+class PPMReceiverSensor : public SensorManager{
+  public:
+  PPMReceiverSensor(int pinNumber, int numberOfChannels);
+  ~PPMReceiverSensor();
+  
+  void Init();
+  void SetPrefix(String prefix);
+  
+  int ReadChannel(int ChannelNumber);
+  
+  String HeaderString() override;
+  String SensorString() override;
+  
+  private:
+  int _pinNumber;
+  int _numberOfChannels;
+  static volatile unsigned long _lastPulseUS;
+  static volatile uint16_t _channel;
+  static volatile int* _channelValues;
+  
+  static PPMReceiverSensor* _instance;
+  const int _MAXPULSEWIDTH = 1800;
+  const int _MINPULSEWIDTH = 400;
+  const int _PULSETHRESHUS = 3000; // Threshold for pulse duration in microseconds
+  String _prefix = "Ch";  
+  
+  void CountPulse();
+  static void ppm_isr(void* arg);  
+  
+
+};
+
+
+#endif
